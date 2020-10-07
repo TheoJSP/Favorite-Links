d61501651d1e4c6f451047bd045607a2908d9fb8
Sp <41648020+TheoJSP@users.noreply.github.com>
2020-10-06 23:13:41 -0300

    2. Mysql, Conexión:
    PART 2 OF 12

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,10 @@
+-- CREAR DATABASE --
+
 CREATE DATABASE database_links;
 
 USE database_links;
+
+-- USERS TABLE --
 
 /*CREA TABLA*/
 CREATE TABLE users(
@@ -11,11 +15,31 @@ CREATE TABLE users(
 );
 
 /*MODIFICA TABLA*/
-ALTER TABLE user 
+ALTER TABLE users 
     ADD PRIMARY KEY (id);
 
-ALTER TABLE user  
+ALTER TABLE users  
     MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 /*LA MUESTRA*/
 DESCRIBE users;
+
+-- LINKS TABLES --
+
+CREATE TABLE links(
+    id INT(11) NOT NULL, 
+    title VARCHAR(150) NOT NULL, 
+    url VARCHAR(255) NOT NULL, 
+    description TEXT,
+    user_id INT(11),
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+); 
+
+ALTER TABLE links
+    ADD PRIMARY KEY(id);
+
+ALTER TABLE links
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE links;
